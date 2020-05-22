c8446613f7765b1e987725f2624ebdec3ca73472
khai96_ <hvksmr1996@gmail.com>
2020-05-22 11:16:57 +0700

    Remove duplicated documentation

--- src/lib.rs
+++ src/lib.rs
@@ -34,31 +34,6 @@ where
     Self: Sized,
 {
     /// Apply `f` to `self`.
-    ///
-    /// **Example:** Same type
-    ///
-    /// ```
-    /// # use pipe_trait::*;
-    /// let inc = |x| x + 1;
-    /// let double = |x| x + x;
-    /// let square = |x| x * x;
-    /// let a = (123i32).pipe(inc).pipe(double).pipe(square);
-    /// let b = square(double(inc(123i32)));
-    /// assert_eq!(a, b);
-    /// ```
-    ///
-    /// **Example:** Type transformation
-    ///
-    /// ```
-    /// # use pipe_trait::*;
-    /// let x = 'x';
-    /// let a = x // char
-    ///     .pipe(|x| (x, x, x)) // (char, char, char)
-    ///     .pipe(|x| [x, x]) // [(char, char, char); 2]
-    ///     .pipe(|x| format!("{:?}", x)); // String
-    /// let b = "[('x', 'x', 'x'), ('x', 'x', 'x')]";
-    /// assert_eq!(a, b);
-    /// ```
     fn pipe<Return>(self, f: impl FnOnce(Self) -> Return) -> Return {
         f(self)
     }
